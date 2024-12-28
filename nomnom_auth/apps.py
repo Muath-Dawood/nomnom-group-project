93447b5a9fe5f09332d37321303055a25569142a
Muath Dawood <muath.dawood.89@gmail.com>
2024-12-28 22:32:30 +0200

    First commit

---
+++ nomnom_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NomnomAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nomnom_auth'
---
+++ nomnom_recipies/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NomnomRecipiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nomnom_recipies'
