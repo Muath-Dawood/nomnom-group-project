365c3ae356ab4cc4716154a2088975ee46ce990f
Admin <baselsaad9999@gmail.com>
2024-12-30 19:16:25 +0200

    messge

--- nomnom_recipies/models.py
+++ nomnom_recipies/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+ 
+class Recipies(models.Model):
+    name = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
