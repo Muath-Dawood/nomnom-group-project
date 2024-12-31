cb8db71fb31629417fe10febcc6dc445fcba985f
Admin <baselsaad9999@gmail.com>
2024-12-31 15:08:43 +0200

    fix recipes name for APP and project

--- nomnom_recipes/apps.py
+++ nomnom_recipes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NomnomRecipiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nomnom_recipies'
+    name = 'nomnom_recipes'
