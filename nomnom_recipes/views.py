f9f8de727fa3bdbc6c4bec1fa77c6a5aca12c269
Admin <baselsaad9999@gmail.com>
2024-12-31 20:55:35 +0200

    added recipes views

--- nomnom_recipes/views.py
+++ nomnom_recipes/views.py
@@ -1,4 +1,100 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 
-def home(req):
-  return render(req, 'recipes/home.html')
+from django.db import IntegrityError
+from django.contrib import messages
+
+from .models import Recipe
+User = get_user_model()
+
+def my_recipes(req):
+  if req.user.is_authenticated:
+    recipes = Recipe.objects.filter(user=req.user.id)
+    return render(req, 'recipe/my_recipes.html', {'recipes': recipes})
+  else:
+    return redirect('/accounts/signin')
+
+def add_recipe(req):
+  if req.user.is_authenticated:
+    post_data = {
+      'title': req.POST['title'],
+      'description': req.POST['description'],
+      'ingredients': req.POST['ingredients'],
+      'instructions': req.POST['instructions'],
+      'image': req.POST['image']
+    #   'image': req.FILES['image'] // is this the right code line for images ?? 
+    }
+    user = User.objects.get(id=req.user.id)
+    errors = Recipe.objects.validate_recipe_data(post_data)
+    try:
+      if len(errors):
+        for value in errors.values():
+          messages.error(req, value)
+        raise ValueError("Messing form fields!")
+      new_show = Recipe(user=user, **post_data)
+      new_show.save()
+    except IntegrityError:
+      messages.error(req, "Name already exists!")
+      return redirect('/add_recipe')
+    except ValueError:
+      return redirect('/add_recipe')
+    else:
+      return redirect('/add_recipe')
+  else:
+    return redirect('/accounts/signin')
+
+def delete_recipe(req, id):
+  Recipe = Recipe.objects.get(id=id)
+  if req.user.is_authenticated:
+    if Recipe.user.id != req.user.id:
+        messages.error(req, "YOU CAN NOT DELETE A RECIPE THAT IS NOT YOURS!")
+        return redirect('/add_recipe')
+    Recipe = Recipe.objects.get(id=id)
+    Recipe.delete()
+  return redirect('/add_recipe')
+
+def show_recipe(req, id):
+  if req.user.is_authenticated:
+    recipe = Recipe.objects.get(id=id)
+    return render(req, 'recipe/show_recipe.html', {'recipe': recipe})
+  else:
+    return redirect('/accounts/signin')
+
+def edit_recipe(req, id):
+  recipe = Recipe.objects.get(id=id)
+  if req.user.is_authenticated:
+    if recipe.user.id != req.user.id:
+      messages.error(req, "YOU CAN NOT EDIT A RECIPE THAT IS NOT YOURS!")
+      return redirect('/edit_recipe')
+    if req.method == 'GET':
+      context = {
+      'recipe': recipe.objects.get(id=id)
+      }
+      return render(req, 'recipe/edit_recipe.html', context)
+    if req.method == 'POST':
+      post_data = {
+        'title': req.POST['title'],
+        'description': req.POST['description'],
+        'ingredients': req.POST['ingredients'],
+        'instructions': req.POST['instructions'],
+        'image': req.POST['image']
+      }
+      errors = Recipe.objects.validate_recipe_data(post_data)
+      try:
+        if len(errors):
+          for value in errors.values():
+            messages.error(req, value)
+          raise ValueError("Messing form fields!")
+        for attr in post_data:
+          if hasattr(recipe, attr):
+            setattr(recipe, attr, post_data[attr])
+        recipe.save()
+      except IntegrityError:
+        messages.error(req, "Name already exists!")
+        return redirect('/edit_recipe')
+      except ValueError:
+        return redirect('/edit_recipe')
+      else:
+        return redirect('/edit_recipe')
+  else:
+    return redirect('/accounts/signin')
