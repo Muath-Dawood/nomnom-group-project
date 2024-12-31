ed2426fa5f8ebcbf4c925d8fa5f6195f74aff166
Admin <baselsaad9999@gmail.com>
2024-12-31 11:12:40 +0200

    add_recipe function

--- nomnom_recipes/views.py
+++ nomnom_recipes/views.py
@@ -1,4 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 
-# Create your views here.
+from django.db import IntegrityError
+from django.contrib import messages
+
+from .models import Recipe
+User = get_user_model()
+
+def add_recipe(req):
+  if req.user.is_authenticated:
+    post_data = {
+      'title': req.POST['title'],
+      'description': req.POST['description'],
+      'ingredients': req.POST['ingredients'],
+      'instructions': req.POST['instructions'],
+      'image': req.POST['image']
+    #   'image': req.FILES['image'] // is this the right code line for images ?? 
+    }
+    user = User.objects.get(id=req.user.id)
+    errors = Recipe.objects.validate_recipe_data(post_data)
+    try:
+      if len(errors):
+        for value in errors.values():
+          messages.error(req, value)
+        raise ValueError("Messing form fields!")
+      new_show = Recipe(user=user, **post_data)
+      new_show.save()
+    except IntegrityError:
+      messages.error(req, "Name already exists!")
+      return redirect('/add_recipe')
+    except ValueError:
+      return redirect('/add_recipe')
+    else:
+      return redirect('/add_recipe')
+  else:
+    return redirect('/accounts/signin')
