9ae0bb36a486bf9aff0cfd932f8319054bd4cc56
Admin <baselsaad9999@gmail.com>
2025-01-01 19:19:53 +0200

    added reviews

--- nomnom_recipes/views.py
+++ nomnom_recipes/views.py
@@ -18,54 +18,63 @@ def my_recipes(req):
   else:
     return redirect('/')
 
+def all_recipes(req):
+    recpies = Recipe.objects.all()
+    return render(req, 'recpies/all_recpies.html', {'recpies': recpies})
+
+
 def add_recipe(req):
   if req.user.is_authenticated:
-    post_data = {
-      'title': req.POST['title'],
-      'description': req.POST['description'],
-      'ingredients': req.POST['ingredients'],
-      'instructions': req.POST['instructions'],
-      'image': req.FILES['image'] 
-    }
-    user = User.objects.get(id=req.user.id)
-    errors = Recipe.objects.validate_recipe_data(post_data)
-    if len(errors):
-        for value in errors.values():
-            messages.error(req, value)
-        return redirect('/add_recipe')    
-    new_recipe = Recipe(created_by=user, **post_data)
-    new_recipe.save()
-    return redirect(f'/recipe/{new_recipe.id}')
-
+    if req.method == "GET":
+      return render(req,'recipes/add_recipe.html')
+    else:
+      post_data = {
+        'title': req.POST['title'],
+        'description': req.POST['description'],
+        'ingredients': req.POST['ingredients'],
+        'instructions': req.POST['instructions'],
+        'image': req.FILES['image'] 
+      }
+      user = User.objects.get(id=req.user.id)
+      errors = Recipe.objects.validate_recipe_data(post_data)
+      if len(errors):
+          for value in errors.values():
+              messages.error(req, value)
+          return redirect('/recipes/add')    
+      new_recipe = Recipe(created_by=user, **post_data)
+      new_recipe.save()
+      return redirect(f'/recipes/my_recipes')
+  else:
+    return redirect('/auth/login')
+  
 def delete_recipe(req, id):
   recipe = Recipe.objects.get(id=id)
   if req.user.is_authenticated:
     if recipe.user.id != req.user.id:
         messages.error(req, "YOU CAN NOT DELETE A RECIPE THAT IS NOT YOURS!")
-        return redirect('/my_recipes')
-    recipe = Recipe.objects.get(id=id)
+        return redirect('/recipes/my_recipes')
     recipe.delete()
-  return redirect('/my_recipes')
-
-def show_recipe(req, id):
-  if req.user.is_authenticated:
-    recipe = Recipe.objects.get(id=id)
-    reviews = recipe.reviews.all()
-    return render(req, 'recipe/show_recipe.html', {'recipe': recipe, 'reviews': reviews})
+    return redirect('/recipes/my_recipes')
   else:
     return redirect('/auth/login')
+
+def show_recipe(req, id):
+    recipe = Recipe.objects.get(id=id)
+    reviews = recipe.reviews.all()
+    return render(req, 'recipes/show_recipe.html', {'recipe': recipe, 'reviews': reviews})
+  
 
 def edit_recipe(req, id):
     recipe = Recipe.objects.get(id=id)
     if req.user.is_authenticated:
         if recipe.user.id != req.user.id:
             messages.error(req, "YOU CAN NOT EDIT A RECIPE THAT IS NOT YOURS!")
-        return redirect('/my_recipes')
+        return redirect('recipes/my_recipes')
     if req.method == 'GET':
         context = {
         'recipe': recipe.objects.get(id=id)
         }
-        return render(req, 'recipe/edit_recipe.html', context)
+        return render(req, 'recipes/edit_recipe.html', context)
     
     if req.method == 'POST':
         post_data = {
@@ -79,7 +88,7 @@ def edit_recipe(req, id):
         if len(errors):
             for value in errors.values():
                 messages.error(req, value)
-            return redirect('/edit_recipe')
+            return redirect(f'recipes/{recipe.id}/edit')
         for attr in post_data:
             if hasattr(recipe, attr):
                 setattr(recipe, attr, post_data[attr])
@@ -91,6 +100,7 @@ def edit_recipe(req, id):
 def add_review(req, id):
     if req.user.is_authenticated:
         recipe = Recipe.objects.get(id=id)
+        user = User.objects.get(id=req.user.id)
         post_data = {
             'rating': req.POST['rating'],
             'comment': req.POST['comment'],
@@ -102,7 +112,7 @@ def add_review(req, id):
             return redirect(f'/recipes/{recipe.id}')
         new_review = Review.objects.create(
             recipe=recipe,
-            reviewer=req.user,
+            reviewer=user,
             rating=post_data['rating'],
             comment=post_data['comment']
         )
@@ -112,12 +122,12 @@ def add_review(req, id):
     else:
         return redirect('/auth/login')
       
-def edit_review(req, review_id):
+def edit_review(req,recipe_id, review_id):
     review = Review.objects.get(id=review_id)
     if req.user.is_authenticated:
         if review.reviewer.id != req.user.id:
             messages.error(req, "YOU CANNOT EDIT A REVIEW THAT IS NOT YOURS!")
-            return redirect(f'/recipes/{review.recipe.id}')
+            return redirect(f'/recipes/{recipe_id}')
 
         if req.method == 'GET':
             context = {
@@ -134,19 +144,23 @@ def edit_review(req, review_id):
             if len(errors):
                 for value in errors.values():
                     messages.error(req, value)
-                return redirect(f'/review/{review.id}/edit/')
+                return redirect(f'/recipes/{recipe_id}/reviews/{review.id}/edit/')
             for attr in post_data:
               if hasattr(review, attr):
                 setattr(review, attr, post_data[attr])
             review.save()
             messages.success(req, "Your review has been updated successfully.")
-            return redirect(f'/recipes/{review.recipe.id}')
+            return redirect(f'/recipes/{recipe_id}')
     else:
         return redirect('/auth/login')
-      
-def all_recipes(req):
+
+def delete_review(req,recipe_id, review_id):
+  review = Review.objects.get(id=review_id)
   if req.user.is_authenticated:
-    recpies = Recipe.objects.all()
-    return render(req, 'recpies/all_recpies.html', {'recpies': recpies})
+    if review.user.id != req.user.id:
+        messages.error(req, "YOU CAN NOT DELETE A REVIEW THAT IS NOT YOURS!")
+        return redirect(f'/recipes/{recipe_id}')
+    review.delete()
+    return redirect(f'/recipes/{recipe_id}')
   else:
     return redirect('/auth/login')
