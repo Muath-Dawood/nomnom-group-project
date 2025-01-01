b99697348cb12dba1672632b8a4f9e40b4b5f8fc
Admin <baselsaad9999@gmail.com>
2025-01-01 18:16:30 +0200

    fix recipes urls

--- nomnom_recipes/urls.py
+++ nomnom_recipes/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='nomnom_recipes_index'),
-    path('all_recipes/', views.all_recipes, name='all_recipes'),
-    path('my_recipes/', views.my_recipes, name='my_recipes'),
-    path('add_recipe/', views.add_recipe, name='add_recipe'),
-    path('recipe/<int:id>/', views.show_recipe, name='show_recipe'),
-    path('recipe/<int:id>/edit/', views.edit_recipe, name='edit_recipe'),
-    path('recipe/<int:id>/delete/', views.delete_recipe, name='delete_recipe'),
-    path('recipe/<int:id>/add_review/', views.add_review, name='add_review'),
-    path('review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('recipes/', views.all_recipes, name='all_recipes'),
+    path('recipes/my_recipes/', views.my_recipes, name='my_recipes'),
+    path('recipes/add/', views.add_recipe, name='add_recipe'),
+    path('recipes/<int:id>/', views.show_recipe, name='show_recipe'),
+    path('recipes/<int:id>/edit/', views.edit_recipe, name='edit_recipe'),
+    path('recipes/<int:id>/delete/', views.delete_recipe, name='delete_recipe'),
+    path('recipes/<int:id>/reviews/add/', views.add_review, name='add_review'),
+    path('recipes/<int:recipe_id>/reviwes/<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('recipes/<int:recipe_id>/reviwes/<int:review_id>/delete/', views.delete_recipe, name='delete_recipe'),
 ]
