81997770dc2477af796147b1cfd32fd752403754
Admin <baselsaad9999@gmail.com>
2025-01-01 14:01:38 +0200

    Added recipes APP urls

--- nomnom_recipes/urls.py
+++ nomnom_recipes/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='nomnom_recipes_index'),
+    path('my_recipes/', views.my_recipes, name='my_recipes'),
+    path('add_recipe/', views.add_recipe, name='add_recipe'),
+    path('recipe/<int:id>/', views.show_recipe, name='show_recipe'),
+    path('recipe/<int:id>/edit/', views.edit_recipe, name='edit_recipe'),
+    path('recipe/<int:id>/delete/', views.delete_recipe, name='delete_recipe'),
+    path('recipe/<int:id>/add_review/', views.add_review, name='add_review'),
+    path('review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
 ]
